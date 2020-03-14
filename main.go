6872354553af6a6a7337016b4b7335e476097021
root <britshindia@gmail.com>
2020-03-14 06:28:09 +0000

    modified the main.go file

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>I'm an awesome Programmer ..!!! ^_^ :)</h1>")
+	fmt.Fprintf(w, "<h1>I'm an good awesome Programmer ..!!! ^_^ :)</h1>")
 	return
 }
 
